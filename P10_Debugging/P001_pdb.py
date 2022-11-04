2edb13a7b59817575473b559bbf76da7e1aefddb
saket61195 <saket61195@gmail.com>
2022-11-04 19:29:50 +0530

    Debugging

---
+++ P10_Debugging/P001_pdb.py
@@ -0,0 +1,30 @@
+import pdb
+def add(num1, num2):
+    pdb.set_trace()
+    t = 4*5
+    print(t)
+    return num1+num2
+
+
+
+add(4, 'five')
+
+
+
+
+
+#(Pdb) help
+
+# Documented commands (type help <topic>):
+# ========================================
+# EOF    c          d        h         list      q        rv       undisplay
+# a      cl         debug    help      ll        quit     s        unt      
+# alias  clear      disable  ignore    longlist  r        source   until    
+# args   commands   display  interact  n         restart  step     up       
+# b      condition  down     j         next      return   tbreak   w        
+# break  cont       enable   jump      p         retval   u        whatis   
+# bt     continue   exit     l         pp        run      unalias  where    
+
+# Miscellaneous help topics:
+# ==========================
+# exec  pdb
---
+++ P10_Debugging/P002_pdb.py
@@ -0,0 +1,13 @@
+'''
+Some useful commands for pdb:
+a : Print the argument list of the current function.
+step: to run the current line of code, and stop at the first possible occasion
+help: to list all the commands available
+help <command>: to see what a command does
+continue: to continue the program till the error comes
+w: previous line, current line and next line content
+next: Continue execution until the next line in the current function is reached or it returns.
+        
+We can change the variables value in the console window as well.
+we can type in the variable name to get its value
+'''
