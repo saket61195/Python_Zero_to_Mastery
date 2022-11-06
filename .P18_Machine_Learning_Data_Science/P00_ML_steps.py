0f5de942551155fcad3ef8511160d4ad80b628d0
saket61195 <saket61195@gmail.com>
2022-11-06 18:17:18 +0530

    Data Science

---
+++ .P18_Machine_Learning_Data_Science/P00_ML_steps.py
@@ -0,0 +1,16 @@
+#1 - Import the data
+#2 - Clean the data
+#3 - Split data. Training Set/Test Set
+#4 - Create a Model 
+#5 - Check the output
+#6 - Improve
+
+
+## Tools in Machine Learning
+
+#1 - Numpy
+#2 - Pandas
+#3 - Scikit-learn
+#4 - Matplotlib
+#5 - Jupyter notebook
+#6 - kaggle
